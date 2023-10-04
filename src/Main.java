150d504277166a7c279921feee31d36aa20a6416
Prariyavit <118101686+Prariyavit@users.noreply.github.com>
2023-10-04 19:28:14 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        TestThrowsException testThrowsException = new TestThrowsException();
+        testThrowsException.testThrows();
+        try {
+            testThrowsException.deposit(-1);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
